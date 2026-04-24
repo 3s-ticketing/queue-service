ff101237954bfeb7517325297653b660ddc854e9
Lim JinKeon <0907john24@gmail.com>
2026-04-24 10:25:46 +0900

    Chore: 초기 설정

---
+++ src/test/java/org/ticketing/queueservice/QueueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ticketing.queueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
