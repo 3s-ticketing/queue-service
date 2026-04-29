71eaec93dd214d4a1f60207f74636074149e80e3
Lim JinKeon <0907john24@gmail.com>
2026-04-29 16:14:45 +0900

    Refactor: 테스트 레디스 설정 수정

--- src/test/java/org/ticketing/queue/QueueServiceApplicationTests.java
+++ src/test/java/org/ticketing/queue/QueueServiceApplicationTests.java
@@ -2,9 +2,31 @@ package org.ticketing.queue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.ticketing.queue.domain.repository.BannedUserRepository;
+import org.ticketing.queue.domain.repository.QueueRedisRepository;
+import org.ticketing.queue.infrastructure.persistence.SseEmitterRepository;
+import org.ticketing.queue.infrastructure.redis.pubsub.QueueRedisSubscriber;
 
 @SpringBootTest
 class QueueServiceApplicationTests {
+
+    // RedisConfig가 RedisConnectionFactory를 찾으므로 MockBean으로 제공
+    @MockitoBean
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @MockitoBean
+    private QueueRedisRepository queueRedisRepository;
+
+    @MockitoBean
+    private QueueRedisSubscriber queueRedisSubscriber;
+
+    @MockitoBean
+    private SseEmitterRepository sseEmitterRepository;
+
+    @MockitoBean
+    private BannedUserRepository bannedUserRepository;
 
     @Test
     void contextLoads() {
--- src/test/java/org/ticketing/queue/config/TestRedisConfig.java
+++ src/test/java/org/ticketing/queue/config/TestRedisConfig.java
@@ -1,11 +1,12 @@
 package org.ticketing.queue.config;
 
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisServerCommands;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
@@ -15,28 +16,28 @@ import static org.mockito.Mockito.mock;
 @TestConfiguration
 public class TestRedisConfig {
 
-    // RedisMessageListenerContainer Mock으로 대체
-    @Bean
-    @Primary
-    public RedisMessageListenerContainer redisMessageListenerContainer() {
-        return mock(RedisMessageListenerContainer.class);
-    }
-
-    // RedisConnectionFactory Mock으로 대체
     @Bean
     @Primary
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisConnectionFactory mock = Mockito.mock(RedisConnectionFactory.class);
-        // RedisConfig의 redisTemplate이 connection() 호출하므로 NPE 방지
-        RedisConnection mockConnection = Mockito.mock(RedisConnection.class);
-        given(mock.getConnection()).willReturn(mockConnection);
-        return mock;
+        LettuceConnectionFactory factory = mock(LettuceConnectionFactory.class);
+        RedisConnection mockConnection = mock(RedisConnection.class);
+        RedisServerCommands mockServerCommands = mock(RedisServerCommands.class);
+
+        given(factory.getConnection()).willReturn(mockConnection);
+        given(mockConnection.serverCommands()).willReturn(mockServerCommands);
+
+        return factory;
     }
 
-    // RedisTemplate Mock으로 대체
     @Bean
     @Primary
     public RedisTemplate<String, String> redisTemplate() {
         return mock(RedisTemplate.class);
+    }
+
+    @Bean
+    @Primary
+    public RedisMessageListenerContainer redisMessageListenerContainer() {
+        return mock(RedisMessageListenerContainer.class);
     }
 }
